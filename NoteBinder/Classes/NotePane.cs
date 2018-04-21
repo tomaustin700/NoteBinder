e7bcd1385987b162d772e28856d55e4a17bac264
tomaustin700 <tomaustin700@gmail.com>
2018-04-21 13:03:37 +0100

    Added view/viewmodel and basic implementation

---
+++ NoteBinder/Classes/NotePane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBinder.Classes
+{
+    public class NotePane
+    {
+        public string Header { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ NoteBinder/ViewModels/NoteBinderViewModel.cs
@@ -0,0 +1,90 @@
+﻿using NoteBinder.Classes;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBinder.ViewModels
+{
+    public class NoteBinderViewModel : BaseViewModel
+    {
+        #region Fields
+        private ObservableCollection<NotePane> _panes;
+        private int _selectedTab;
+
+        #endregion
+
+
+
+        public int SelectedTab
+        {
+            get { return _selectedTab; }
+            set
+            {
+                _selectedTab = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        #region Properties
+        public ObservableCollection<NotePane> Panes
+        {
+            get { return _panes; }
+            set
+            {
+                _panes = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public NoteBinderViewModel()
+        {
+            SaveCommand = new DelegateCommand(Save);
+            OpenCommand = new DelegateCommand(Open);
+            NewCommand = new DelegateCommand(New);
+
+        }
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand SaveCommand { get; set; }
+        public DelegateCommand OpenCommand { get; set; }
+        public DelegateCommand NewCommand { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override void Loaded()
+        {
+            Panes = new ObservableCollection<NotePane>();
+            Panes.Add(new NotePane() { Header = "Test Header", Notes = "Lorem ipsum blah blah blah" });
+            Panes.Add(new NotePane() { Header = "Test Header2", Notes = "Lorem ipsum blah blah blah" });
+
+        }
+
+        public void New()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Open()
+        {
+
+        }
+        #endregion
+    }
+}
