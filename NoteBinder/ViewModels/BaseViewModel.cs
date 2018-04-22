9d4d625d3f8d90dfb9fdb6b62f5dc5f7cee9835d
Tom Austin <tom.austin@streets-heaver.com>
2018-04-22 21:29:51 +0100

    Tidied slightly

--- NoteBinder/ViewModels/BaseViewModel.cs
+++ NoteBinder/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NoteBinder.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,21 +9,9 @@ using System.Threading.Tasks;
 
 namespace NoteBinder.ViewModels
 {
-    public abstract class BaseViewModel : INotifyPropertyChanged
+    public abstract class BaseViewModel : BaseNotify
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // This method is called by the Set accessor of each property.
-        // The CallerMemberName attribute that is applied to the optional propertyName
-        // parameter causes the property name of the caller to be substituted as an argument.
-        public void RaisePropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
+       
         public BaseViewModel()
         {
             Loaded();
